6734cd98b015db7dc87c2e4edd9f42df9ec566d3
Jayesh Purohit <jayeshpurohit456789@gmail.com>
2023-06-25 16:46:32 +0530

    safearea added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import TabBar from './src/components/tab-bar';
+import {SafeAreaView} from 'react-native/types';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <TabBar />
+      <SafeAreaView style={{flex: 1}}>
+        <TabBar />
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 };
